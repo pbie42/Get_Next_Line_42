f27e1d162674b038ea7d398af3c353ccd6aaf93d
Paul BIE <pbie@e1r9p14.42.fr>
2016-01-28 18:01:09 +0100

    Get_Next_Line

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,63 @@
 /*   By: pbie <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/27 15:02:39 by pbie              #+#    #+#             */
-/*   Updated: 2016/01/27 17:39:27 by pbie             ###   ########.fr       */
+/*   Updated: 2016/01/28 18:00:41 by pbie             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include "libft.h"
+#include <unistd.h>
+
+char			*ft_strnjoin(char const *s1, char const *s2, size_t n)
+{
+	int			i;
+	int			j;
+	char		*str;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = 0;
+	j = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + n + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != 0 && j < n)
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
 
 int				get_next_line(int const fd, char **line)
 {
-	char		buff[BUFF_SIZE];
+	char		buf[BUFF_SIZE + 1];
+	int			ret;
+	char		*sav;
+	char		*c;
+	char		*tmp;
 
-	if (fd < 0 || !line || read(fd, buff, 0) < 0)
+	ret = 1;
+	if (fd < 0 || line == NULL || read(fd, buf, 0) < 0)
 		return (-1);
+	while (c = ft_strchr(tmp, '\n') == NULL, ret)
+	{
+		ret = read(fd, buf, BUFF_SIZE);
+		buf[ret] = '\0';
+		sav = tmp;
+		if ((tmp = ft_strjoin(line, buf)) == NULL)
+			return (-1);
+		free(sav);
+	}
+	ft_strncpy(line, tmp, c - tmp - 1);
+	if (ret == 0)
+		return (0);
+	return (1);
 }
